cf1420886339f6035c692752c3d653f70dc3d67d
arianalima10 <ariana.crodrigues@gmail.com>
2025-11-04 23:04:15 -0300

    Mostrando Alert - ERROR

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -12,6 +12,6 @@ public class ViewController {
 	
 	@FXML
 	public void onbtnClickAction() {
-		Alerts.showAlert("Alert title", "Alert header", "Hello", AlertType.INFORMATION);
+		Alerts.showAlert("Alert title", "Alert header", "Hello", AlertType.ERROR);
 	}
 }
